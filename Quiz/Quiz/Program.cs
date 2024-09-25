fde413df3e1c8ac1f9ea0937e1146d949b8c9c1c
Florescu Denis <denistaur@gmail.com>
2024-09-25 09:07:29 +0300

    .
    .

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -26,7 +26,7 @@ namespace Quiz
             Intrebare[] intrebari = new Intrebare[nrLinii];
             string[] rezultat = c1.GetIntrebari(caleFisier5);
             string[] rezultat1= c1.GetRaspunsuri(caleFisier5);
-             
+            
             for(int i = 0;i<nrLinii;i++)
             {
                 intrebari[i] = new Intrebare(rezultat[i], rezultat1[i]);
